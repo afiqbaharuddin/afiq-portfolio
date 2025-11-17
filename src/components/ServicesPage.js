7fd2b7a53ee74a1f5fe4c865c6043c3e7b114c59
afiqbaharuddin <mafqqq16@gmail.com>
2025-11-17 17:46:44 +0800

    update google sheet

--- src/components/ServicesPage.js
+++ src/components/ServicesPage.js
@@ -115,7 +115,7 @@ const ServicesPage = () => {
     };
 
     const sendToGoogleSheets = async (data) => {
-      const scriptURL = 'https://script.google.com/macros/s/AKfycbzAeYoQqTfUrNJQRCFESnRf-ecuOS0omBcbFcFA-CcxhPJ2xdj6Ywaem7qy0ZG-QOtgrg/exec'; // You'll need to set this up
+      const scriptURL = 'https://script.google.com/macros/library/d/1xt8DK-E1z8FZruOX3ZTCxAtxBHtEhv5LFL43ZayfWjnWB8liHqmRFHo3/1'; // You'll need to set this up
       
       try {
         const response = await fetch(scriptURL, {
